2da434ea3a56e735927c9406607165a26805cdb7
Peter Jacobo <object.opera@gmail.com>
2020-09-06 21:21:40 -0400

    Fetching Initial Posts

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,28 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Hello from express!');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: 'f32a23ff3',
+      title: 'First server-side post',
+      content: 'This is coming from the server'
+    },
+    {
+      id: '9132rh7r12',
+      title: 'Second server-side post',
+      content: 'This is coming from the server'
+    },
+    {
+      id: '23ta03t23a',
+      title: 'Third server-side post',
+      content: 'This is coming from the server'
+    },
+  ];
+  res.status(200).json({
+    message: 'Posts fetched succesfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
