7168a32b34922337749e18314c3e774916db6f0e
Peter Jacobo <object.opera@gmail.com>
2020-09-11 17:58:40 -0400

    Fetching Data from a Database

--- backend/app.js
+++ backend/app.js
@@ -42,27 +42,13 @@ app.post('/api/posts', (req, res, next) => {
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: 'f32a23ff3',
-      title: 'First server-side post',
-      content: 'This is coming from the server'
-    },
-    {
-      id: '9132rh7r12',
-      title: 'Second server-side post',
-      content: 'This is coming from the server'
-    },
-    {
-      id: '23ta03t23a',
-      title: 'Third server-side post',
-      content: 'This is coming from the server'
-    },
-  ];
-  res.status(200).json({
-    message: 'Posts fetched succesfully!',
-    posts: posts
-  });
+  Post.find()
+    .then(documents => {
+      res.status(200).json({
+        message: 'Posts fetched succesfully!',
+        posts: documents
+      });
+    });
 });
 
 module.exports = app;
