c6841338dcbbc079aa0514f63fae9c86b8bd4de0
Peter Jacobo <object.opera@gmail.com>
2020-09-10 20:14:00 -0400

    Understanding Mongoose Schemas & Models

---
+++ backend/models/post.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const postSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true}
+});
+
+mongoose.model('Post', postSchema);
+
+module.exports = mongoose.model('Post', postSchema);
