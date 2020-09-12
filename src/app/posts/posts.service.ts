e209d879bd4cb4b1a23c12631ca2a7cf22734e99
Peter Jacobo <object.opera@gmail.com>
2020-09-12 00:15:42 -0400

    Finish the Edit Feature

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -34,9 +34,13 @@ export class PostsService {
     return this.postsUpdated.asObservable();
   }
 
+  getPost(id: string) {
+    return this.http.get<{_id: string; title: string; content: string}>("http://localhost:3000/api/posts/" + id);
+  }
+
   addPost(title: string, content: string) {
     const post: Post = {id: null, title: title, content: content};
-    this.http.post<{message: string, postId: string }>('http://localhost:3000/api/posts', post)
+    this.http.post<{message: string; postId: string }>('http://localhost:3000/api/posts', post)
       .subscribe((responseData) => {
         const id = responseData.postId;
         post.id = id;
@@ -45,8 +49,20 @@ export class PostsService {
       })
   }
 
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = { id: id, title: title, content: content };
+    this.http.put("http://localhost:3000/api/posts/" + id, post)
+      .subscribe(response => {
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex(p => p.id === id);
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+
   deletePost(postId: string) {
-    this.http.delete("http://localhost:3000/api/posts/" + postId )
+    this.http.delete("http://localhost:3000/api/posts/" + postId)
       .subscribe(() => {
         const updatedPosts = this.posts.filter(post => post.id !== postId);
         this.posts = updatedPosts;
